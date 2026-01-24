3fb3d6c81d2fa7527c2ea051a215748c07c0c057
expy <nikerezov@gmail.com>
2026-01-24 09:10:07 -0500

    no panicon callstack query for non-module

--- src/windows_platform/callstack.rs
+++ src/windows_platform/callstack.rs
@@ -106,7 +106,7 @@ pub fn get_call_stack(
         // Validate that the instruction pointer is within a loaded module
         // Don't issue a warning if there is less than 2 modules (main executable is only loaded when process is started, but address is in ntdll)
         if modules.len() > 1 && !is_valid_instruction_pointer(instruction_pointer, &modules) {
-            panic!("Instruction pointer 0x{:016x} not in any loaded module. Including frame without symbols.", instruction_pointer);
+            trace!("Instruction pointer 0x{:016x} not in any loaded module. Including frame without symbols.", instruction_pointer);
         }
         
         // Resolve symbol information
